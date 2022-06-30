5f61ae9e403b6bc653accbef906fa6449b7c3680
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2022-06-30 16:25:10 +0900

    change LogRecord class to struct

--- include/sharksfin/LogRecord.h
+++ include/sharksfin/LogRecord.h
@@ -26,7 +26,7 @@
 
 namespace sharksfin {
 
-enum class LogOperation : std::uint64_t {
+enum class LogOperation : std::uint32_t {
     UNKNOWN = 0U,
     INSERT,
     UPDATE,
@@ -61,99 +61,18 @@ inline std::ostream& operator<<(std::ostream& out, LogOperation value) {
 /**
  * @brief Log record entry
  * @details This object represents a log record entry that is collected and shipped to OLAP.
- * This class is intended to trivially copyable type and implementation provides compatible class and they can be
- * used interchangeably by memcpy or reinterpret_cast.
+ * This class is intended to be trivially copyable type and implementation provides compatible class and they are
+ * interchangeable by memcpy or reinterpret_cast.
  */
-class LogRecord {
-
-public:
+struct LogRecord {
     using storage_id_type = StorageOptions::storage_id_type;
 
-    /**
-     * @brief create empty object
-     */
-    LogRecord() = default;
-
-    /**
-     * @brief destruct object
-     */
-    ~LogRecord() = default;
-
-    /**
-     * @brief copy constructor
-     */
-    LogRecord(LogRecord const& other) = default;
-
-    /**
-     * @brief copy assignment
-     */
-    LogRecord& operator=(LogRecord const& other) = default;
-
-    /**
-     * @brief move constructor
-     */
-    LogRecord(LogRecord&& other) noexcept = default;
-
-    /**
-     * @brief move assignment
-     */
-    LogRecord& operator=(LogRecord&& other) noexcept = default;
-
-    /**
-     * @brief accessor to the key data
-     * @return string view referencing the key data
-     */
-    [[nodiscard]] std::string_view key() const noexcept {
-        return key_;
-    }
-
-    /**
-     * @brief accessor to the value data
-     * @return string view referencing the value data
-     */
-    [[nodiscard]] std::string_view value() const noexcept {
-        return value_;
-    }
-
-    /**
-     * @brief accessor to the major version
-     * @return the major version number of this log entry
-     */
-    [[nodiscard]] std::uint64_t major_version() const noexcept {
-        return major_version_;
-    }
-
-    /**
-     * @brief accessor to the minor version
-     * @return the minor version number of this log entry
-     */
-    [[nodiscard]] std::uint64_t minor_version() const noexcept {
-        return minor_version_;
-    }
-
-    /**
-     * @brief accessor to the storage id
-     * @return the storage id where the log record was made
-     */
-    [[nodiscard]] storage_id_type storage_id() const noexcept {
-        return storage_id_;
-    }
-
-    /**
-     * @brief accessor to the operation type code
-     * @return the operation type of this log record
-     */
-    [[nodiscard]] LogOperation operation() const noexcept {
-        return operation_;
-    }
-
-private:
+    LogOperation operation_{};
     std::string_view key_{};
     std::string_view value_{};
     std::uint64_t major_version_{};
     std::uint64_t minor_version_{};
     storage_id_type storage_id_{};
-    LogOperation operation_{};
 };
 
 static_assert(std::is_trivially_copyable_v<LogRecord>);
